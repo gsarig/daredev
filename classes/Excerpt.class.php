1ab7829971e580dbaa57cb00401228ca0fa32441
gsarig <contact@gsarigiannidis.gr>
2016-05-09 01:32:18 +0300

    Improved the excerpt

--- classes/Excerpt.class.php
+++ classes/Excerpt.class.php
@@ -1,38 +1,40 @@
-<?php 
+<?php
 
 namespace DareDev;
 
 class Excerpt {
-    
-    public static function limit($new_length = null, $more = true, $string = null, $id = null ) {
-        /**
-        * Custom excerpt which allows you to set the length.
-        * Usage: echo \DareDev\Excerpt::limit(40, 'more...');
-        *
-        */
-        $id = ($id) ? $id : get_the_ID();
-        $permalink = get_permalink($id);
-        $get_excerpt = ($string) ? $string : get_the_excerpt();
-        $excerpt = self::shorten($get_excerpt, $new_length);
-        $more_txt = ($more && is_string($more)) ? $more : 'Read more';
-        $more    = ($more) ? '<a href="'.$permalink.'" class="more" rel="bookmark">' . $more_txt . '</a>' : '';
-        $excerpt = ($excerpt) ? '<p>' . $excerpt . $more . '</p>' : ''; 
-        
-        return $excerpt;
-    }
-    public static function shorten($s, $num) {
-        if ($num < mb_strlen($s)) {
-                $fs = mb_substr($s, 0, $num);
 
-                for ($i = mb_strlen($fs); $i >= 0; $i--) {
-                    if (mb_substr($fs, $i, 1) == ' ')
-                        return mb_substr($fs, 0, $i+1).'...';
-                    }
+	public static function limit( $new_length = null, $more = true, $string = null, $id = null, $dots = '...' ) {
+		/**
+		 * Custom excerpt which allows you to set the length.
+		 * Usage: echo \DareDev\Excerpt::limit(40, 'more...');
+		 *
+		 */
+		$id          = ( $id ) ? $id : get_the_ID();
+		$permalink   = get_permalink( $id );
+		$get_excerpt = ( $string ) ? $string : get_the_excerpt( $id );
+		$excerpt     = self::shorten( $get_excerpt, $new_length, $dots );
+		$more_txt    = ( $more && is_string( $more ) ) ? $more : 'Read more';
+		$more        = ( $more ) ? '<a href="' . $permalink . '" class="more" rel="bookmark">' . $more_txt . '</a>' : '';
+		$excerpt     = ( $excerpt ) ? '<p>' . $excerpt . $more . '</p>' : '';
 
-                    return $fs.'...';
+		return $excerpt;
+	}
 
-        } else {
-                return $s;
-        }
-    }
+	public static function shorten( $s, $num, $dots ) {
+		if ( $num < mb_strlen( $s ) ) {
+			$fs = mb_substr( $s, 0, $num );
+
+			for ( $i = mb_strlen( $fs ); $i >= 0; $i -- ) {
+				if ( mb_substr( $fs, $i, 1 ) == ' ' ) {
+					return mb_substr( $fs, 0, $i + 1 ) . $dots;
+				}
+			}
+
+			return $fs . $dots;
+
+		} else {
+			return $s;
+		}
+	}
 }
