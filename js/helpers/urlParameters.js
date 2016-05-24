8048afcd899659465b31d6d22220f0d75242f590
gsarig <contact@gsarigiannidis.gr>
2016-05-24 22:59:11 +0300

    Get URL Parameters

---
+++ js/helpers/urlParameters.js
@@ -0,0 +1,20 @@
+/**
+ * Get URL Parameters (eg. http://example.com/?filter-name=some-value)
+ * Use it like this:
+ * getUrlParameter('filter-name');
+ */
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
