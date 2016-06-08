b1717531ba845cbd746a964b2337f7ac8a450523
gsarig <contact@gsarigiannidis.gr>
2016-06-08 14:05:16 +0300

    Fixes to menu toggle

--- classes/Menu.class.php
+++ classes/Menu.class.php
@@ -2,9 +2,11 @@
 /**
  * Mobile menu
  * Use it like that:
- * echo \DareDev\Menu::toggle();
+ * $menu = new \DareDev\Menu();
+ * echo $menu->toggle();
  * or
- * echo \DareDev\Menu::toggle('#ffffff', '#000000', 'My Menu', 'my-menu');
+ * $menu = new \DareDev\Menu::toggle('#ffffff', '#000000', 'My Menu', 'my-menu');
+ * echo $menu->toggle();
  */
 
 namespace DareDev;
@@ -12,11 +14,23 @@ namespace DareDev;
 
 class Menu {
 
-	public function toggle( $initial_color = '#000000', $clicked_color = '#666666', $text = 'Menu', $text_class = '' ) {
-		self::styles( $initial_color, $clicked_color );
-		add_action( 'wp_enqueue_scripts', array( $this, 'styles' ) );
+	public $initial_color;
+	public $clicked_color;
+	public $text;
+	public $text_class;
 
-		$text_class = ( $text_class ) ? $text_class : 'screen-reader-text';
+	public function __construct( $initial_color = '#000000', $clicked_color = '#666666', $text = 'Menu', $text_class = '' ) {
+		$this->initial_color = $initial_color;
+		$this->clicked_color = $clicked_color;
+		$this->text          = $text;
+		$this->text_class    = $text_class;
+		self::styles();
+		add_action( 'wp_enqueue_scripts', array( $this, 'styles' ) );
+	}
+
+	public function toggle() {
+
+		$text_class = ( $this->text_class ) ? $this->text_class : 'screen-reader-text';
 
 		return '<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
 					<span class="menu-icon">
@@ -24,11 +38,11 @@ class Menu {
 						<span class="second"></span>
 						<span class="third"></span>
 					</span>
-					<span class="' . $text_class . '">' . $text . '</span>
+					<span class="' . $text_class . '">' . $this->text . '</span>
 				</button>';
 	}
 
-	private function styles( $initial_color, $clicked_color ) {
+	private function styles() {
 		wp_enqueue_style(
 			'daredev-menu-styles',
 			plugin_dir_url( __DIR__ ) . 'css/custom.css'
@@ -43,7 +57,7 @@ class Menu {
 				}
 				
 				.main-navigation.toggled .menu-toggle > .menu-icon > span {
-					background: {$clicked_color};
+					background: {$this->clicked_color};
 				}
 				
 				.menu-toggle > .menu-icon > span {
@@ -51,7 +65,7 @@ class Menu {
 					height: 3px;
 					width: 100%;
 					margin: 5px 0;
-					background: {$initial_color};
+					background: {$this->initial_color};
 					-webkit-transition: all 0.5s ease;
 					transition: all 0.5s ease;
 				}
