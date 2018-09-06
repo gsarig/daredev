df78dd4b895b6de98c492ce9ea7e4a25b6af2fb6
gsarig <contact@gsarigiannidis.gr>
2018-09-07 00:31:45 +0300

    Improving removeAccents function

--- daredev/classes/Transform.class.php
+++ daredev/classes/Transform.class.php
@@ -2,34 +2,45 @@
 
 namespace DareDev;
 
-class Transform
-{
+class Transform {
 
-	public static function removeAccents( $string )
-	{
+	public static function removeAccents( $string ) {
 		$text = [
-			'Ά' => 'Α',
-			'Έ' => 'Ε',
-			'Ή' => 'Η',
-			'Ί' => 'Ι',
-			'Ό' => 'Ο',
-			'Ύ' => 'Υ',
-			'Ώ' => 'Ω',
-			'ά' => 'α',
-			'έ' => 'ε',
-			'ή' => 'η',
-			'ί' => 'ι',
-			'ό' => 'ο',
-			'ύ' => 'υ',
-			'ώ' => 'ω',
-			'ΰ' => 'ϋ',
-			'ΐ' => 'ϊ',
+			'Ά'  => 'Α',
+			'Έ'  => 'Ε',
+			'Ή'  => 'Η',
+			'Ί'  => 'Ι',
+			'Ό'  => 'Ο',
+			'Ύ'  => 'Υ',
+			'Ώ'  => 'Ω',
+			'ά'  => 'α',
+			'έ'  => 'ε',
+			'ή'  => 'η',
+			'ί'  => 'ι',
+			'ό'  => 'ο',
+			'ύ'  => 'υ',
+			'ώ'  => 'ω',
+			'ΰ'  => 'ϋ',
+			'ΐ'  => 'ϊ',
+			'ΆΙ' => 'ΑΪ',
+			'ΆΥ' => 'ΑΫ',
+			'ΈΙ' => 'ΕΪ',
+			'ΌΙ' => 'ΟΪ',
+			'ΈΥ' => 'ΕΫ',
+			'ΌΥ' => 'ΟΫ',
+			'άι' => 'αϊ',
+			'έι' => 'εϊ',
+			'Άυ' => 'αϋ',
+			'άυ' => 'αϋ',
+			'όι' => 'οϊ',
+			'Έυ' => 'εϋ',
+			'έυ' => 'εϋ',
+			'όυ' => 'οϋ',
+			'Όυ' => 'οϋ',
 		];
 
-		foreach ( $text as $key => $value )
-		{
-			if ( $key !== FALSE && $key >= 0 )
-			{
+		foreach ( $text as $key => $value ) {
+			if ( $key ) {
 				$string = str_replace( $key, $value, $string );
 			}
 		}
@@ -37,8 +48,7 @@ class Transform
 		return $string;
 	}
 
-	public static function format( $text, $wrapper = '%%', $before = '<span>', $after = '</span>' )
-	{
+	public static function format( $text, $wrapper = '%%', $before = '<span>', $after = '</span>' ) {
 		$match = preg_match(
 			'/(?<=' . $wrapper . ')[^' . $wrapper . ']+(?=' . $wrapper . ')/', $text, $matches
 		);
@@ -51,8 +61,7 @@ class Transform
 	/*
 	* Get the content with formatting
 	*/
-	public static function post_content_with_formatting( $content )
-	{
+	public static function post_content_with_formatting( $content ) {
 		$content = apply_filters( 'the_content', $content );
 		$content = str_replace( ']]>', ']]&gt;', $content );
 
