0a8fedfeff67cf36840017e64f223af3f30ef383
gsarig <contact@gsarigiannidis.gr>
2020-05-09 13:39:31 +0300

    Simplified Menu toggler.

--- daredev/classes/Menu.class.php
+++ daredev/classes/Menu.class.php
@@ -13,22 +13,15 @@ namespace DareDev;
 
 
 class Menu {
-
-	public $initial_color;
-	public $clicked_color;
 	public $text;
 	public $text_class;
 
 	public function __construct(
-		$initial_color = '#000000',
-		$clicked_color = '#666666',
 		$text = 'Menu',
 		$text_class = ''
 	) {
-		$this->initial_color = $initial_color;
-		$this->clicked_color = $clicked_color;
-		$this->text          = $text;
-		$this->text_class    = $text_class;
+		$this->text       = $text;
+		$this->text_class = $text_class;
 		self::styles();
 		add_action( 'wp_enqueue_scripts', array( $this, 'styles' ) );
 	}
@@ -50,51 +43,7 @@ class Menu {
 	private function styles() {
 		wp_enqueue_style(
 			'daredev-menu-styles',
-			WPMU_PLUGIN_URL . '/daredev/css/custom.css'
+			WPMU_PLUGIN_URL . '/daredev/css/menu-toggle.css'
 		);
-		$menu_css = "
-				.menu-toggle > .menu-icon {
-					display: inline-block;
-					width: 40px;
-					height: 36px;
-					padding: 5px 7px;
-					border-radius: 3px;
-				}
-				
-				.main-navigation.toggled .menu-toggle > .menu-icon > span {
-					background: {$this->clicked_color};
-				}
-				
-				.menu-toggle > .menu-icon > span {
-					display: block;
-					height: 3px;
-					width: 100%;
-					margin: 5px 0;
-					background: {$this->initial_color};
-					-webkit-transition: all 0.5s ease;
-					transition: all 0.5s ease;
-				}
-				
-				.main-navigation.toggled .menu-icon > .first {
-					-webkit-transform: rotate(45deg);
-					transform: rotate(45deg);
-					margin-left: 0;
-					margin-top: 14px;
-					width: 27px;
-				}
-				
-				.main-navigation.toggled .menu-icon > .second {
-					-webkit-transform: rotate(-45deg);
-					transform: rotate(-45deg);
-					margin-left: 0;
-					margin-top: -8px;
-					width: 27px;
-				}
-				
-				.main-navigation.toggled .menu-icon > .third {
-					display: none;
-				}
-                ";
-		wp_add_inline_style( 'daredev-menu-styles', $menu_css );
 	}
 }
